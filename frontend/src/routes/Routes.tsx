45e216a842be7f98669a46487ec856269dfb9ef4
Mfrozzz <marcosvinibva@gmail.com>
2024-12-12 14:23:08 -0300

    adding routes, pages, adapting some files

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -1,13 +1,16 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { Routes , Route, } from 'react-router-dom'
+import RecipesPage from '../pages/RecipesPage';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
 
-function Routes(){
+function RoutesApp(){
     return (
-        <BrowserRouter>
-            <Route></Route>
-            <Route></Route>
-            <Route></Route>
-        </BrowserRouter>
+        <Routes>
+            <Route path='/' element={<RecipesPage/>}/>
+            <Route path='/signin' element={<SignIn/>}/>
+            <Route path='/signup' element={<SignUp/>}/>
+        </Routes>
     );
 }
 
-export default Routes;
+export default RoutesApp;
