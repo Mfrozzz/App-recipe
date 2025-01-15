a42c4299b82a7ec51eb7da1c381025943ed4e90b
Mfrozzz <marcosvinibva@gmail.com>
2025-01-15 16:01:28 -0300

    refactor SignIn component and styles for improved layout and user experience

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import { SignInService } from "../service/SigninUserService";
+import styles from "../pages/css/SignIn.module.css";
+import { BsBoxArrowInDownLeft } from "react-icons/bs";
+import { FaSignInAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-function SignIn(){
+function SignIn() {
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
 
@@ -16,30 +20,46 @@ function SignIn(){
     };
 
     return (
-        <div>
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Email:</label>
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
+        <>
+            <nav>
+                <div className={styles.leftNav}>
+                    Tasty🥐Pick
                 </div>
-                <div>
-                    <label>Password:</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
+                <div className={styles.rightNav}>
+                    {/* arrumar alinhamento icons */}
+                    <span className={styles.btnsRight}><FaSignInAlt /> <Link to="/signin">Sign in</Link></span>
+                    <span className={styles.btnsRight}><BsBoxArrowInDownLeft /> <Link to="/signup">Sign up</Link></span>
                 </div>
-                <button type="submit">Login</button>
-            </form>
-        </div>
+            </nav>
+            <body className={styles.bodySignIn}>
+                <div className={styles.loginContainer}>
+                    <h2>Login</h2>
+                    <form onSubmit={handleSubmit} className={styles.formContainer}>
+                        <div className={styles.formGroup}>
+                            <label className={styles.labelLogin}>Email:</label>
+                            <input
+                                type="email"
+                                value={email}
+                                className={styles.inputLogin}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className={styles.formGroup}>
+                            <label className={styles.labelLogin}>Password:</label>
+                            <input
+                                type="password"
+                                value={password}
+                                className={styles.inputLogin}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <button type="submit" className={styles.loginBtn}>Login</button>
+                    </form>
+                </div>
+            </body>
+        </>
     );
 };
 
